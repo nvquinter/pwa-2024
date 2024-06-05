89b94330b6a147ff91d7cf055e0188a5afcb24b1
Vale Quintero <valuquintero@gmail.com>
2024-06-05 18:49:30 +0200

    agregue estilos

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from './pages/Home/Home';
+import Characters from './pages/Characters/Characters';
+import Contact from './pages/Contact/Contact';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+         <Router>
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/characters' element={<Characters/>}/>
+                <Route path='/contact' element={<Contact/>}/>
+            </Routes>
+        </Router>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
